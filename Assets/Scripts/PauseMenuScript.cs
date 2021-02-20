2be1edd65af1e2b508060f8ba7afd97e866f064c
Chika0IQ <mohdkhairin62@gmail.com>
2021-02-20 22:08:06 +0800

    Beta Submission

---
+++ Assets/Scripts/PauseMenuScript.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PauseMenuScript : MonoBehaviour
+{
+
+    public static bool GamePaused = false;
+
+
+    public GameObject pauseMenuUI;
+    public Slider volSlider;
+    public Toggle muteChk;
+
+    public AudioSource bgmAudio;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //bgmAudio = GameObject.FindGameObjectWithTag("BgmAudio");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(GamePaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GamePaused = false;
+    }
+
+    public void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GamePaused = true;
+        if(GamePaused == true)
+        {
+            if (!muteChk.isOn)
+            {
+                Debug.Log("NotChecked");
+                
+            }
+            if(muteChk.isOn)
+            {
+                Debug.Log("Checked");
+                bgmAudio.mute = !bgmAudio.mute;
+            }
+        }
+        
+    }
+
+
+}
